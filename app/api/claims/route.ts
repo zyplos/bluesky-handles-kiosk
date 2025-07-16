04a20c7bb9698209743b05efe563121ae6738003
Angel <22824872+Zyplos@users.noreply.github.com>
2025-07-16 03:45:25 -0500

    claim form: grab hostname, better validation

--- app/api/claims/route.ts
+++ app/api/claims/route.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { isStringEmpty } from "@/internals/utils";
+import { isStringEmpty, rootDomains } from "@/internals/utils";
 import { NextResponse, type NextRequest } from "next/server";
 
 export interface HandleFormState {
@@ -8,10 +8,35 @@ export interface HandleFormState {
   errors: string[];
 }
 
+// https://atproto.com/specs/handle#handle-identifier-syntax
 const HANDLE_REGEX =
   /^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$/;
 
-export async function POST(req: NextRequest) {
+// https://atproto.com/specs/did#at-protocol-did-identifier-syntax
+const DID_REGEX = /^did:[a-z]+:[a-zA-Z0-9._:%-]*[a-zA-Z0-9._-]$/;
+
+export async function POST(
+  req: NextRequest
+): Promise<NextResponse<HandleFormState>> {
+  const host = req.headers.get("host");
+  if (!host) {
+    return NextResponse.json(
+      { message: "Sorry, couldn't process your request.", errors: [] },
+      { status: 400 }
+    );
+  }
+
+  const hostname = host.split(":")[0];
+  if (!rootDomains.includes(hostname) && !hostname.includes("localhost")) {
+    return NextResponse.json(
+      {
+        message: `Sorry, ${hostname} doesn't seem configured correctly to take handle claims.`,
+        errors: [],
+      },
+      { status: 400 }
+    );
+  }
+
   const formData = await req.formData();
   const handle = formData.get("handleString")?.toString().trim();
   const did = formData.get("didString")?.toString().trim();
@@ -22,18 +47,26 @@ export async function POST(req: NextRequest) {
 
   const errors: string[] = [];
 
+  const handleWithHostname = `${handle}.${hostname}`;
   if (
     !handle ||
-    // handle.includes(".") ||
-    // !HANDLE_REGEX.test(handle) ||
-    handle.length >= 100
+    isStringEmpty(handle) ||
+    handle.length >= 100 ||
+    handle.includes(".") ||
+    !HANDLE_REGEX.test(handleWithHostname)
   ) {
     errors.push(
       "Invalid handle. It cannot contain spaces or periods and must be less than 100 characters. It can only contain numbers, letters or dashes (-) and cannot contain unicode characters."
     );
   }
 
-  if (!did || !did.startsWith("did:") || isStringEmpty(did)) {
+  if (
+    !did ||
+    isStringEmpty(did) ||
+    did.length >= 300 ||
+    !did.startsWith("did:") ||
+    DID_REGEX.test(did)
+  ) {
     errors.push(`Invalid DID. Make sure it starts with "did:"`);
   }
 
@@ -44,5 +77,8 @@ export async function POST(req: NextRequest) {
     );
   }
 
-  return NextResponse.json({ message: `claimed ${handle}`, errors: [] });
+  return NextResponse.json({
+    message: `claimed ${handleWithHostname}`,
+    errors: [],
+  });
 }
