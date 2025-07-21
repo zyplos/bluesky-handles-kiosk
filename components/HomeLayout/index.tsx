97b2c9ea5f7425a826a004b8996a57bcdb9c496a
Angel <22824872+Zyplos@users.noreply.github.com>
2025-07-21 17:58:27 -0500

    HomeLayout: restructure to allow different stuff in footer

--- components/HomeLayout/index.tsx
+++ components/HomeLayout/index.tsx
@@ -1,26 +1,45 @@
-import { PageButton } from "../Button";
 import styles from "./styles.module.scss";
 
 export interface HomeLayoutProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
 }
 
-export default function HomeLayout({
-  children,
-  className,
-  ...props
-}: HomeLayoutProps) {
+export function HomeLayout({ children, className, ...props }: HomeLayoutProps) {
   return (
     <div
       id="root"
       className={`${styles.rootWrapper} ${className || ""}`}
       {...props}
     >
-      <div className={styles.centeredContent}>{children}</div>
-
-      <footer className={styles.footerInfo}>
-        <PageButton href="/auth/signin">Claim a Bluesky Handle</PageButton>
-      </footer>
+      {children}
     </div>
+  );
+}
+
+//
+
+export function CenteredContent({
+  children,
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className={`${styles.centeredContent} ${className || ""}`} {...props}>
+      {children}
+    </div>
+  );
+}
+
+//
+
+export function FooterContent({
+  children,
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <footer className={`${styles.footerInfo} ${className || ""}`} {...props}>
+      {children}
+    </footer>
   );
 }
